30aba4e7c0a389b20669001495ae2b8802c7e3ad
iMac <imac@iMacs-iMac-Pro.local>
2021-07-23 10:21:36 +0530

    Commit all code

---
+++ myportfolio/lib/data/skills.dart
@@ -0,0 +1,20 @@
+class Skill {
+  final String name;
+
+  Skill({this.name});
+}
+
+// ignore: non_constant_identifier_names
+List<Skill> SKILLS = [
+ // Skill(name: 'Java'),
+  Skill(name: 'Flutter'),
+  Skill(name: 'Dart'),
+  Skill(name: 'React.js'),
+  Skill(name: 'React Native'),
+  Skill(name: 'Xamarin.forms'),
+  Skill(name: 'Xamarin Android'),
+  Skill(name: 'Php'),
+  Skill(name: 'Java Script'),
+  Skill(name: 'NodeJs'),
+  Skill(name: 'Git'),
+];
